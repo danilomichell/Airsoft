be7a787a2fb17177b9d7652104fb6b33d9fd13bb
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-03-08 16:05:34 -0300

    Implementação do método Singleton

--- Airsoft.Singleton/Program.cs
+++ Airsoft.Singleton/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airsoft.Singleton
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AluguelAirsoft aluguel = AluguelAirsoft.GetInstancia();
+
+            var aluguel1 = aluguel.criarAluguelFuzil();
+            var aluguel2 = aluguel.criarAluguelPistolal();
+            var aluguel3 = aluguel.criarAluguelFuzilPrecisao();
+
+            aluguel.ArmasAlugadas(aluguel1);
+            aluguel.ArmasAlugadas(aluguel2);
+            aluguel.ArmasAlugadas(aluguel3);
+        }
+    }
+}
