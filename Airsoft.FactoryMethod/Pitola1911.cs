28ccd6c513b635ad2d296572b3791685d9290b5a
paulo <paulo@PROTECH-DESK-AC>
2022-03-23 13:14:32 -0300

    pequenos ajustes no FactoryMethod

--- Airsoft.FactoryMethod/Pitola1911.cs
+++ Airsoft.FactoryMethod/Pitola1911.cs
@@ -4,6 +4,6 @@ public class Pitola1911 : IArma
 {
     public string exibirInfo()
     {
-        return "Nome: GTP9 \nTipo: Pistola \nFábrica: Amorer Works \nPreço: R$35 \nAno: 1911";
+        return "Informações da arma: \n"+"Nome: GTP9 \nTipo: Pistola \nFábrica: Amorer Works \nPreço: R$35 \nAno: 1911\n";
     }
 }
