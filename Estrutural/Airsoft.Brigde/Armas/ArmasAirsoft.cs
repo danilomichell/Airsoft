c369eaa561dca68db4d22b49b7dea8af68c70e98
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-03-30 10:48:16 -0300

    Ajuestes peço aluguel

---
+++ Estrutural/Airsoft.Brigde/Armas/ArmasAirsoft.cs
@@ -0,0 +1,27 @@
+﻿using Airsoft.Bridge.Fabricas;
+
+namespace Airsoft.Bridge.Armas;
+
+public class ArmasAirsoft : IArmas
+{
+    public ArmasAirsoft(string nome, string tipo, FabricaArmas fabrica, double preco, DateTime ano)
+    {
+        Nome = nome;
+        Tipo = tipo;
+        Fabrica = fabrica;
+        Preco = preco;
+        Ano = ano;
+    }
+
+    public string Nome { get; set; }
+    public string Tipo { get; set; }
+    public FabricaArmas Fabrica { get; set; }
+    public double Preco { get; set; }
+    public DateTime Ano { get; set; }
+
+
+    public string ExibirInfo()
+    {
+        return $"Nome: {Nome} \nTipo: {Tipo} \nFábrica: {Fabrica.Nome} \nPreço: R${Preco} \nAno: {Ano}";
+    }
+}
--- Estrutural/Airsoft.Brigde/Client.cs
+++ Estrutural/Airsoft.Brigde/Client.cs
@@ -4,6 +4,7 @@ namespace Airsoft.Bridge;
 
 internal class Client
 {
+    public double ValorTotal { get; set; }
     public void Main()
     {
         ClientMethod(new FabricaArmas());
@@ -13,13 +14,17 @@ internal class Client
     {
         var prodA = new FabricaArmas {Nome = "G&G"};
         var pistola = prodA.CriarPistola("GTP9", "Pistola", prodA, 70, DateTime.Now);
+        ValorTotal += pistola.Preco;
 
         Console.WriteLine(pistola.ExibirInfo());
         Console.WriteLine();
 
         var rifle = prodA.CriarRifle("GTP9", "Rifle", prodA, 70, DateTime.Now);
+        ValorTotal += rifle.Preco;
 
         Console.WriteLine(rifle.ExibirInfo());
         Console.WriteLine();
+
+        Console.WriteLine($"Valor total do aluguel: R${ValorTotal}");
     }
 }
